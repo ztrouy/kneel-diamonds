7fcf10db80323fef011c6277e7cbd53b47533d89
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-12 16:23:00 -0600

    Wrote function to generate HTML for styles

---
+++ scripts/styleOptions.js
@@ -0,0 +1,12 @@
+export const StyleOptions = async () => {
+    const response = await fetch("http://localhost:8088/styles")
+    const styles = await response.json()
+
+
+    let optionsHTML = ""
+    for (const style of styles) {
+        optionsHTML += `<div><input type="radio" name="style" value="${style.id}" /> ${style.style}</div>`
+    }
+
+    return optionsHTML
+}
