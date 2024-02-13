d11a970c125786990766730ff5c4d92c9b2205bb
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-13 11:32:28 -0600

    Wrote function that generates HTML for all placed orders

---
+++ scripts/ordersList.js
@@ -0,0 +1,14 @@
+export const ordersList = async () => {
+    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=size&_expand=style")
+    const orders = await response.json()
+
+    const ordersArray = orders.map(order => {
+        const orderPrice = order.metal.price + order.size.price + order.style.price
+
+        return `<div>Order #${order.id} cost $${orderPrice.toFixed(0)}</div>`
+    })
+
+    const ordersHTML = ordersArray.join("")
+
+    return ordersHTML
+}
