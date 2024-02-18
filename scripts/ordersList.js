0825ef402bfe8dac070c12011b4cf04bd6428fc0
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-18 15:25:22 -0600

    Updated price calculations to include jewelry type

--- scripts/ordersList.js
+++ scripts/ordersList.js
@@ -1,9 +1,9 @@
 export const ordersList = async () => {
-    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=size&_expand=style")
+    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=size&_expand=style&_expand=jewelry")
     const orders = await response.json()
 
     const ordersArray = orders.map(order => {
-        const orderPrice = order.metal.price + order.size.price + order.style.price
+        const orderPrice = (order.metal.price + order.size.price + order.style.price) * order.jewelry.priceMultiplier
 
         return `<div>Order #${order.id} cost $${orderPrice.toFixed(2)}</div>`
     })
